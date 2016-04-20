31f83c0365abd5fce2fa8e80162de28704f7518b
jooshka <jooshka@mail.ru>
2016-04-21 01:42:36 +0300

    Update default paths

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,7 +4,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Lk Lunch"
+  config.site_title = "Smart Lunch"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
@@ -105,6 +105,8 @@ ActiveAdmin.setup do |config|
   #
   # Default:
   # config.logout_link_method = :get
+
+  config.logout_link_method = :delete
 
   # == Root
   #

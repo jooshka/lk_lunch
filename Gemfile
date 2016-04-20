bded0a220cb7580af65129646fdc7239d959b362
jooshka <jooshka@mail.ru>
2016-04-21 01:10:47 +0300

    Add ActiveAdmin

--- Gemfile
+++ Gemfile
@@ -49,3 +49,4 @@ end
 gem 'devise'
 gem 'bootstrap-sass'
 gem 'slim-rails'
+gem 'activeadmin', github: 'activeadmin'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root 'home#index'
 
-  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

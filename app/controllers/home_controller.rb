83f04f968796a8209658d426cba3fb2b7664c33d
jooshka <jooshka@mail.ru>
2016-04-22 17:33:01 +0300

    Designed week days page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
+
   def index
+    @start_date = valid_date(params[:date]).monday
   end
+
+  private
+
+    def valid_date(a_string)
+      Date.parse(a_string)
+    rescue
+      Date.current
+    end
+
 end
